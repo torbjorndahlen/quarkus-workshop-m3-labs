6940cf0b1991517b6ef8177a54e5b73f2c5afe24
Torbjörn Dahlén <torbjorn.dahlen@icloud.com>
2022-05-17 14:21:15 +0200

    updated deploy scripts

--- scripts/deploy-all.sh
+++ scripts/deploy-all.sh
@@ -1,25 +1,11 @@
 #!/bin/bash
 
-USERXX=$1
 
-if [ -z "$USERXX" -o "$USERXX" = "userXX" ]
-  then
-    echo "Usage: Input your username like deploy-all.sh user1"
-    exit;
-fi
 
-echo Your username is $USERXX
-
-oc delete deployments,dc,bc,build,svc,route,pod,is --all
-echo "Waiting 30 seconds to finialize deletion of resources..."
-sleep 30
-
-oc project $USERXX-project
-
-sh $CHE_PROJECTS_ROOT/quarkus-workshop-m3-labs/scripts/deploy-vets-service.sh
-sh $CHE_PROJECTS_ROOT/quarkus-workshop-m3-labs/scripts/deploy-customers-service.sh
-sh $CHE_PROJECTS_ROOT/quarkus-workshop-m3-labs/scripts/deploy-visits-service.sh
-sh $CHE_PROJECTS_ROOT/quarkus-workshop-m3-labs/scripts/deploy-petclinic-web-v2.sh
+sh deploy-vets-service.sh
+sh deploy-customers-service.sh
+sh deploy-visits-service.sh
+sh deploy-petclinic-web-v2.sh
 
 oc rollout status -w dc/vets-service &&
   oc rollout status -w dc/customers-service &&

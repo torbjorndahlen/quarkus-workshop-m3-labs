2106fe300062a5691b2a66040351b0cb38450738
Torbjörn Dahlén <torbjorn.dahlen@icloud.com>
2022-05-24 14:23:44 +0200

    native build

--- scripts/deploy-quarkus-petclinic.sh
+++ scripts/deploy-quarkus-petclinic.sh
@@ -2,16 +2,16 @@
 
 echo Deploy Pet Clinic........
 
-oc delete all -l app=petclinic-database
+#oc delete all -l app=petclinic-database
 
-oc new-app openshift/postgresql:10-el8 \
-            --name=petclinic-database \
-            -e POSTGRESQL_USER=petclinic \
-            -e POSTGRESQL_PASSWORD=mysecretpassword \
-            -e POSTGRESQL_DATABASE=petclinic 
+#oc new-app openshift/postgresql:10-el8 \
+#            --name=petclinic-database \
+#           -e POSTGRESQL_USER=petclinic \
+#            -e POSTGRESQL_PASSWORD=mysecretpassword \
+#            -e POSTGRESQL_DATABASE=petclinic 
 
-mvn clean package -DskipTests -f $CHE_PROJECTS_ROOT/quarkus-workshop-m3-labs/quarkus-petclinic
-#mvn clean package -Pnative -DskipTests -f $CHE_PROJECTS_ROOT/quarkus-workshop-m3-labs/quarkus-petclinic-vets-service
+#mvn clean package -DskipTests -f $CHE_PROJECTS_ROOT/quarkus-workshop-m3-labs/quarkus-petclinic
+mvn clean package -Pnative -DskipTests -f $CHE_PROJECTS_ROOT/quarkus-workshop-m3-labs/quarkus-petclinic
 
-oc label deployment/petclinic-database app.openshift.io/runtime=postgresql --overwrite && \
-oc label deployment/petclinic-database app.kubernetes.io/part-of=quarkus-petclinic --overwrite
+#oc label deployment/petclinic-database app.openshift.io/runtime=postgresql --overwrite && \
+#oc label deployment/petclinic-database app.kubernetes.io/part-of=quarkus-petclinic --overwrite
